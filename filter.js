7c96680efa5a6490be9eb7e3cc5832e20bde12fe
Panthutk <panthat.k@gmail.com>
2022-11-14 01:10:06 +0700

    build basic

---
+++ filter.js
@@ -0,0 +1,6 @@
+// choose colour
+const grade10 = document.getElementById("grade10")
+const colors = ['red','white']
+grade10.addEventListener('click',function onClick() {
+    grade10.style.backgroundColor = "salmon";   
+})
